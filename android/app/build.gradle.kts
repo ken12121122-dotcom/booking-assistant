b4f9149b88462b87af534625d4dac7cf7b05fd8a
ken12121122-dotcom <ken12121122@gmail.com>
2026-08-16 22:46:56 +0800

    Bump Android app to v0.4.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.bookingassistant.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     buildFeatures {

930795cfe3bd42bdc555440b73faa1e0ee4f7deb
ken12121122-dotcom <ken12121122@gmail.com>
2026-08-16 22:03:13 +0800

    Target stable Android SDK 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.bookingassistant.app"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.bookingassistant.app"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
